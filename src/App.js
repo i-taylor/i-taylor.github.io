5e869327b1e12418d24a22da5079819bac33ba3d
Isaac <isaaccamerontaylor@gmail.com>
2024-05-15 15:33:25 -0400

    added navigation and cleaned up a bit, need to add animations and flow to scrolling

--- src/App.js
+++ src/App.js
@@ -24,38 +24,26 @@ export default function App() {
         <div className="left">
           <h1>Isaac Taylor</h1>
           <h2>Software Developer, Innovator, and Life-long Learner</h2>
-          <h3>
-            I'm Isaac Taylor, a motivated individual with a strong academic
+          <ul className="navigation">
+            <li className="nav-items"><a href= "#about">About</a>  </li>
+            <li className="nav-items" > <a href= "#experience">Experience</a> </li>
+            <li className="nav-items"> <a href= "#projects">Projects</a> </li>
+
+          </ul>
+        </div>
+        <div className="right">
+        <h2 id="about">About</h2>  
+        <div className="summary">
+        <p>I'm Isaac Taylor, a motivated individual with a strong academic
             background and a keen interest in computer science and cognitive
             science. Currently pursuing a Bachelor of Science with a double
             major in Computer Science and Cognitive Science at the University of
-            Michigan.
-            <br />
-            <br />
-            In my role as a Learning Experience Design Fellow at the Center for
-            Academic Innovation in Ann Arbor, MI, I've had the opportunity to
-            contribute significantly to the enhancement of the Web Design for
-            Everybody course offered through the University of Michigan's online
-            learning platform. From organizing and building the online course to
-            managing database upkeep and testing virtual reality experiences,
-            I've been deeply involved in the course's development process.
-            <br />
-            <br />
-            Additionally, I've explored and beta-tested new data visual tools
-            and served as a UX designer for a UMICH faculty course. My
-            proficiency extends beyond academic and professional realms—I've
-            also worked on various projects that showcase my technical skills.
-            From implementing queue-based algorithms in projects like Zombie
-            Tower Defence to utilizing machine learning in Piazza ML, I've
-            demonstrated my ability to tackle complex problems with creativity
-            and innovation. Projects like Among Us have allowed me to refine my
-            skills in optimization and pathfinding algorithms.
-          </h3>
+            Michigan.</p>
+
         </div>
-        <div className="right">
-        <h2>Experience - <a href="https://drive.google.com/file/d/12tXrb8a6JFbpSzl_TV_-dALdG5EdQCXn/view?usp=sharing" target="_blank">Resume</a></h2>
-          <br />
-          <div className="summary">
+
+        <h2 id="experience">Experience - <a href="https://drive.google.com/file/d/12tXrb8a6JFbpSzl_TV_-dALdG5EdQCXn/view?usp=sharing" target="_blank">Resume</a></h2>
+          <div className="experience" >
           <h3>Learning Experience Design Fellow</h3>
           <h4>May 2023 - May 2024</h4>
           <p>During my fellowship, I contributed to the development and enhancement of the "Web Design for Everybody" course offered through the University of Michigan's online learning platform. This included organizing and aiding in the construction of the course on Coursera, managing database maintenance for course materials, PowerPoints, and readings, and ensuring content currency throughout iterations. Additionally, I conducted thorough testing of virtual reality experiences, providing guidance on error correction and fostering discussions for improvement. I also explored and beta-tested new data visualization tools for the Learning Experience Design (LXD) team, providing training and support to team members. Furthermore, I served as a UX designer for a UMICH faculty course, participating in the creation of mockups, wireframes, and HTML production. Additionally, I was responsible for creating and managing Canvas Courses for graduate classes at the University of Michigan.
@@ -65,8 +53,8 @@ export default function App() {
           <h3>Course Operations Fellow</h3>
           <h4>May 2024 - Current</h4>
           <p>I currently assist in the production and maintenance of UMICH online courses. With a focus in upcoming AI and ML mini courses, I help to ensure courses are up to standars, are free of errors, and continue to keep information updated. I also assist in the process of new iterations, which involve a cross-functional team - including faculty and professors, to iterate and update course matieral of previously released courses.</p>
-          <h2>Projects</h2>
-          <div class="glass-card">
+          <h2 id="projects">Projects</h2>
+          <div class="glass-card" >
             <img src="your-image-url.jpg" alt=""></img>
             <h3><a href="https://github.com/i-taylor/EECS-442-FINAL-PROJECT" target="_blank">ASL Finger Spelling ML Classifier</a></h3>
             <p>
