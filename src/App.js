5d1c30686c181cffae9cf2a476b202ba071e8e94
Isaac <isaaccamerontaylor@gmail.com>
2024-05-17 14:49:35 -0400

    added some animation while hovering over nav and cleaned up some jsx

--- src/App.js
+++ src/App.js
@@ -60,34 +60,91 @@ export default function App() {
 
         </div>
 
-        <h2 id="experience">Experience - <a href="https://drive.google.com/file/d/12tXrb8a6JFbpSzl_TV_-dALdG5EdQCXn/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a></h2>
+        <h2 id="experience">Experience - 
+          <a 
+            href="https://drive.google.com/file/d/12tXrb8a6JFbpSzl_TV_-dALdG5EdQCXn/view?usp=sharing" 
+            target="_blank" 
+            rel="noreferrer">
+              Resume
+          </a>
+        </h2>
           <div className="experience" >
-          <h3>Learning Experience Design Fellow</h3>
-          <h4>May 2023 - May 2024</h4>
-          <p>During my fellowship, I contributed to the development and enhancement of the "Web Design for Everybody" course offered through the University of Michigan's online learning platform. This included organizing and aiding in the construction of the course on Coursera, managing database maintenance for course materials, PowerPoints, and readings, and ensuring content currency throughout iterations. Additionally, I conducted thorough testing of virtual reality experiences, providing guidance on error correction and fostering discussions for improvement. I also explored and beta-tested new data visualization tools for the Learning Experience Design (LXD) team, providing training and support to team members. Furthermore, I served as a UX designer for a UMICH faculty course, participating in the creation of mockups, wireframes, and HTML production. Additionally, I was responsible for creating and managing Canvas Courses for graduate classes at the University of Michigan.
-          </p>
+            <h3>Learning Experience Design Fellow</h3>
+            <h4>May 2023 - May 2024</h4>
+            <p>During my fellowship, I contributed to the development 
+              and enhancement of the "Web Design for Everybody" course 
+              offered through the University of Michigan's online learning 
+              platform. This included organizing and aiding in the construction 
+              of the course on Coursera, managing database maintenance for 
+              course materials, PowerPoints, and readings, and ensuring 
+              content currency throughout iterations. Additionally, I conducted 
+              thorough testing of virtual reality experiences, providing 
+              guidance on error correction and fostering discussions for 
+              improvement. I also explored and beta-tested new data 
+              visualization tools for the Learning Experience Design (LXD) team, 
+              providing training and support to team members. Furthermore, I 
+              served as a UX designer for a UMICH faculty course, participating 
+              in the creation of mockups, wireframes, and HTML production. 
+              Additionally, I was responsible for creating and managing Canvas 
+              Courses for graduate classes at the University of Michigan.
+            </p>
           </div>
          
-          <h3>Course Operations Fellow</h3>
-          <h4>May 2024 - Current</h4>
-          <p>I currently assist in the production and maintenance of UMICH online courses. With a focus in upcoming AI and ML mini courses, I help to ensure courses are up to standars, are free of errors, and continue to keep information updated. I also assist in the process of new iterations, which involve a cross-functional team - including faculty and professors, to iterate and update course matieral of previously released courses.</p>
+         <div className="experience"> 
+            <h3>Course Operations Fellow</h3>
+            <h4>May 2024 - Current</h4>
+            <p>I currently assist in the production and maintenance of 
+              UMICH online courses. With a focus in upcoming AI and ML 
+              mini courses, I help to ensure courses are up to standars, 
+              are free of errors, and continue to keep information updated. 
+              I also assist in the process of new iterations, which involve 
+              a cross-functional team - including faculty and professors, 
+              to iterate and update course matieral of previously released 
+              courses.
+            </p>
+          </div>
           <h2 id="projects">Projects</h2>
           <div class="glass-card" >
             <img src="your-image-url.jpg" alt=""></img>
-            <h3><a href="https://github.com/i-taylor/EECS-442-FINAL-PROJECT" target="_blank" rel="noreferrer">ASL Finger Spelling ML Classifier</a></h3>
+            <h3>
+              <a 
+                href="https://github.com/i-taylor/EECS-442-FINAL-PROJECT" 
+                target="_blank" 
+                rel="noreferrer">
+                  ASL Finger Spelling ML Classifier
+              </a>
+            </h3>
             <p>
-            Developed an ASL Finger Spelling Recognition program using machine learning techniques to facilitate real-time translation of American Sign Language (ASL) gestures into written text. Custom dataset creation, algorithm optimization, and emphasis on accessibility underscored our commitment to inclusive communication solutions. The Developed an ASL Finger Spelling Recognition program using machine learning techniques to facilitate real-time translation of American Sign Language (ASL) gestures into written text. Custom dataset creation, algorithm optimization, and emphasis on accessibility underscored our commitment to inclusive communication solutions.
+            Developed an ASL Finger Spelling Recognition program using 
+            machine learning techniques to facilitate real-time translation 
+            of American Sign Language (ASL) gestures into written text. Custom 
+            dataset creation, algorithm optimization, and emphasis on accessibility 
+            underscored our commitment to inclusive communication solutions. The 
+            Developed an ASL Finger Spelling Recognition program using machine 
+            learning techniques to facilitate real-time translation of American 
+            Sign Language (ASL) gestures into written text. Custom dataset creation, 
+            algorithm optimization, and emphasis on accessibility underscored our 
+            commitment to inclusive communication solutions.
             </p>
           </div>
           <br />
           <br />
           <div class="glass-card">
             <img src="your-image-url.jpg" alt=""></img>
-            <h3><a href="https://github.com/i-taylor/BIOLOGY102_FINAL" target="_blank" rel="noreferrer">AI Tomato Disease Classifier</a></h3>
+            <h3>
+              <a 
+              href="https://github.com/i-taylor/BIOLOGY102_FINAL" 
+              target="_blank" 
+              rel="noreferrer">
+                AI Tomato Disease Classifier
+              </a>
+            </h3>
             <p>
-            An artificial intelligence (AI) tool to identify and diagnose diseases affecting tomato plants. By utilizing machine learning algorithms trained on vast datasets of images depicting healthy and diseased tomato plants, the classifier can accurately distinguish between different types of diseases, including blight, leaf mold, early blight, and more.
-
-
+            An artificial intelligence (AI) tool to identify and diagnose diseases 
+            affecting tomato plants. By utilizing machine learning algorithms trained 
+            on vast datasets of images depicting healthy and diseased tomato plants, 
+            the classifier can accurately distinguish between different types of diseases, 
+            including blight, leaf mold, early blight, and more.
             </p>
           </div>
           <br />
@@ -96,7 +153,9 @@ export default function App() {
             <img src="your-image-url.jpg" alt=""></img>
             <h3>This website!</h3>
             <p>
-              This website has been my biggest project lately as I teach myself react, and touch up my css skills, and is still in progress, if you see this - welcome! 
+              This website has been my biggest project lately as I teach myself 
+              react, and touch up my css skills, and is still in progress, 
+              if you see this - welcome! 
               <br />
               Placeholder for sumary.
             </p>
